5caa4e961b4877f2bacd5755f80cd931872c5e7f
David Justo <david.justo.1996@gmail.com>
2019-07-25 16:15:03 -0700

    incomplete test file for scalar operations

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import numpy
+from morpheus.morpheus import NormalizedTable # TODO: make organization
+
+def test_scalar_by_matrix_ops():
+    normTable = NormalizedTable("", "", "")
+    #TODO: test addition, multiplication, and element-wise log, exp
+    #TODO: use a unit testing framework....can we even do this?
+
+if __name__ == '__main__':
+    test_scalar_by_matrix_ops()
+
+
